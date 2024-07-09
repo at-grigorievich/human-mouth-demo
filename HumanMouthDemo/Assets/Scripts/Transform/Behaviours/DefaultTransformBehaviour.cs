cc14b3811edd0fb61b5ff52ee7665e20a44cec2d
Alexander Tsukanov <at.grigorievich@gmail.com>
2024-07-09 16:54:11 +0300

    1. Добавлен курсор 2. Добавлено ограничение поворота камеры

--- HumanMouthDemo/Assets/Scripts/Transform/Behaviours/DefaultTransformBehaviour.cs
+++ HumanMouthDemo/Assets/Scripts/Transform/Behaviours/DefaultTransformBehaviour.cs
@@ -6,9 +6,11 @@ namespace ATG.Transform
 {
     public sealed class DefaultTransformBehaviour : ITransformBehaviour
     {
+        public static float YRotateAxisRange = 80f;
+
         private readonly UnityTransform _transform;
         private readonly TransformData _config;
-        
+
         public UnityTransform Transform => _transform;
 
         public DefaultTransformBehaviour(UnityTransform transform, TransformData config)
@@ -24,7 +26,7 @@ namespace ATG.Transform
 
             Vector3 nextTarget = _transform.position + forward + right;
 
-            _transform.position = Vector3.MoveTowards(_transform.position, nextTarget, 
+            _transform.position = Vector3.MoveTowards(_transform.position, nextTarget,
                                                 _config.SetPositionSpeed * Time.deltaTime);
         }
 
@@ -32,8 +34,21 @@ namespace ATG.Transform
         {
             Vector3 rotateVector = _transform.localEulerAngles + new Vector3(-mousePos.y, mousePos.x, 0f);
 
-            _transform.localRotation = Quaternion.RotateTowards(_transform.localRotation, Quaternion.Euler(rotateVector), 
+            Quaternion res = Quaternion.RotateTowards(_transform.localRotation, Quaternion.Euler(rotateVector),
                                                                             _config.SetRotationSpeed * Time.deltaTime);
+
+            Vector3 clampedEulerAngles = res.eulerAngles;
+
+            clampedEulerAngles.x = ClampAngle(clampedEulerAngles.x, -YRotateAxisRange, YRotateAxisRange);
+
+            _transform.localEulerAngles = clampedEulerAngles;
+
+            float ClampAngle(float angle, float from, float to)
+            {
+                if (angle < 0f) angle = 360 + angle;
+                if (angle > 180f) return Mathf.Max(angle, 360 + from);
+                return Mathf.Min(angle, to);
+            }
         }
     }
 }
